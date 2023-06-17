69f8daf50122d3970e84c63ed4c93c068d93a04d
Weslei7x <132898177+Weslei7xis@users.noreply.github.com>
2023-06-17 09:46:03 -0300

    Add files via upload

---
+++ revisao.js
@@ -0,0 +1,32 @@
+//LAÇOS DE REPETIÇÃO
+
+//numeros impares de 0 a 100, exemplo de for
+/*for (let i = 0; i < 100; i++ ){
+    if(i % 2 !== 0){
+        document.write(i)
+    }
+}*/
+
+
+// exemplo de Do While
+/*let resposta = "";
+do{
+    resposta = prompt("vc é maior de idade?:");
+}while(resposta.toLocaleLowerCase()!= 'sim');*/
+
+
+// esxemplo de forEach
+/*['cobra', 'coelho', 'tartaruga'].forEach(function(item){
+document.write(item)
+})*/
+
+//exemplo while
+
+/*let numero = prompt("Digite um numero:")
+
+let i = 1;
+while(i<=10){
+    let result = numero * i;
+    console.log(result);
+    i++
+}*/
